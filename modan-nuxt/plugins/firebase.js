4aa15cd8f181056f1557012736497656ab6716b9
kijima masaru <kijima4510@gmail.com>
2023-10-18 16:26:43 +0900

    firebaseと連携

---
+++ modan-nuxt/plugins/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyBA7EMEkNBOzonEIqWhzrUCoH5y4ar_tLw",
+        authDomain: "modan-2aa4d.firebaseapp.com",
+        projectId: "modan-2aa4d",
+        storageBucket: "modan-2aa4d.appspot.com",
+        messagingSenderId: "736494956024",
+        appId: "1:736494956024:web:b6573a4210af39e34bfa0d",
+        measurementId: "G-6Y5RQNE04H"
+    });
+}
+
+export default firebase;
