e4c2cbe5b5a3e6b131f9614e81e57c5c92721ea3
kijima masaru <kijima4510@gmail.com>
2023-10-18 20:24:50 +0900

    register/SideNav.vueの修正

---
+++ modan-nuxt/components/SideNav.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="side-nav">
+    <div class="title">
+      SHARE
+    </div>
+    <div>
+      <router-link to="/">ホーム</router-link>
+    </div>
+    <button @click="logout">ログアウト</button>
+    <textarea v-model="postContent" placeholder="投稿内容"></textarea>
+    <button @click="submitPost">シェアする</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      postContent: ''
+    };
+  },
+  methods: {
+    logout() {
+      // ログアウト処理
+    },
+    submitPost() {
+      // 投稿内容をサーバーに送信する処理
+    }
+  }
+}
+</script>
+
+<style scoped>
+.side-nav {
+  width: 30%; /* 例として、全体の30%を使用 */
+  background-color: #1A1A2E;
+  padding: 20px;
+  box-shadow: 2px 0px 5px rgba(0, 0, 0, 0.2);
+  height: 100vh;
+}
+
+.title {
+  font-size: 24px;
+  font-weight: bold;
+  color: #E94560;
+  margin-bottom: 20px;
+}
+
+router-link {
+  display: block;
+  color: #FFF;
+  text-decoration: none;
+  margin-bottom: 10px;
+  font-size: 18px;
+}
+
+button {
+  display: block;
+  background-color: #E94560;
+  color: #FFF;
+  border: none;
+  padding: 10px 20px;
+  margin-top: 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+button:hover {
+  background-color: #D43F50;
+}
+
+textarea {
+  width: 100%;
+  height: 120px;
+  padding: 10px;
+  margin-top: 20px;
+  border: none;
+  border-radius: 5px;
+  resize: none;
+}
+</style>
--- modan-nuxt/pages/register.vue
+++ modan-nuxt/pages/register.vue
@@ -1,59 +1,131 @@
 <template>
-    <div class="register">
-        <label
-        >メールアドレス： <input v-model="email" type="email" required
-        /></label>
-        <br />
-        <label
-        >パスワード： <input v-model="password" type="password" required
-        /></label>
-        <br />
-        <button @click="register">新規登録</button>
-        <br />
-        <NuxtLink to="/">戻る</NuxtLink>
+  <div class="container">
+    <div class="title-container">
+      <img src="path_to_your_image" alt="Share" class="title-image"/> <!-- ① -->
     </div>
+    <div class="nav-links">
+      <a href="/register">新規登録</a> <!-- ② -->
+      <a href="/login">ログイン</a> <!-- ③ -->
+    </div>
+    <div class="registration-container">
+      <h2>新規登録タイトル</h2> <!-- ④ -->
+      <input type="text" placeholder="ユーザーネーム" v-model="username"/> <!-- ⑤ -->
+      <input type="email" placeholder="メールアドレス" v-model="email"/> <!-- ⑥ -->
+      <input type="password" placeholder="パスワード" v-model="password"/> <!-- ⑦ -->
+      <button @click="register">新規登録</button> <!-- ⑧ -->
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
 import firebase from "~/plugins/firebase";
+
 export default {
-    data() {
-        return {
-            email: null,
-            password: null,
-        };
-    },
-    methods: {
-        register() {
-            if (!this.email || !this.password) {
-                alert("メールアドレスまたはパスワードが入力されていません。");
-                return;
+  data() {
+    return {
+      username: "",
+      email: null,
+      password: null,
+    };
+  },
+  methods: {
+    async register() {
+      if (!this.email || !this.password || !this.username) {
+        alert("ユーザーネーム、メールアドレスまたはパスワードが入力されていません。");
+        return;
+      }
+      
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(this.email, this.password);
+        const user = firebase.auth().currentUser;
+        
+        await user.sendEmailVerification();
+        
+        // Firebaseの認証に成功した後、Laravelのエンドポイントにリクエスト
+        const response = await axios.post('/api/register', {
+            username: this.username,
+            email: this.email,
+            password: this.password
+        });
+
+        if(response.data.message === 'Successfully registered!') {
+            this.$router.replace("/confirm");
         }
-        firebase
-            .auth()
-            .createUserWithEmailAndPassword(this.email, this.password)
-            .then((data) => {
-                data.user.sendEmailVerification().then(() => {
-                    this.$router.replace("/confirm");
-                });
-            })
-            .catch((error) => {
-                switch (error.code) {
-            case "auth/invalid-email":
-                alert("メールアドレスの形式が違います。");
-                break;
-            case "auth/email-already-in-use":
-                alert("このメールアドレスはすでに使われています。");
-                break;
-            case "auth/weak-password":
-                alert("パスワードは6文字以上で入力してください。");
-                break;
-            default:
-                alert("エラーが起きました。しばらくしてから再度お試しください。");
-                break;
-                }
-            });
-        },
+      } catch (error) {
+        switch (error.code) {
+          case "auth/invalid-email":
+            alert("メールアドレスの形式が違います。");
+            break;
+          case "auth/email-already-in-use":
+            alert("このメールアドレスはすでに使われています。");
+            break;
+          case "auth/weak-password":
+            alert("パスワードは6文字以上で入力してください。");
+            break;
+          default:
+            alert("エラーが起きました。しばらくしてから再度お試しください。");
+            break;
+        }
+      }
     },
+  },
 };
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Arial, sans-serif; /* シンプルなフォントスタイル */
+}
+.title-container {
+  margin-top: 50px;
+}
+.title-image {
+  width: 150px;
+  height: auto;
+}
+.nav-links {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center; /* 中央揃え */
+  gap: 20px;
+}
+.nav-links a {
+  color: #333; /* ダークグレーの色 */
+  text-decoration: none;
+  letter-spacing: 2px; /* 文字の間隔を調整 */
+}
+.nav-links a:hover {
+  text-decoration: underline;
+}
+.registration-container {
+  margin-top: 50px;
+  width: 340px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px; /* 項目間のスペースを増やす */
+}
+input[type="text"], input[type="email"], input[type="password"] {
+  padding: 12px; /* パディングを増やす */
+  border: 2px solid #ccc; /* 枠線を少し太くする */
+  border-radius: 4px;
+  font-size: 18px; /* フォントサイズを増やす */
+}
+button {
+  padding: 12px 25px; /* パディングを増やす */
+  background-color: purple;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  font-size: 18px; /* フォントサイズを増やす */
+  cursor: pointer;
+}
+button:hover {
+  background-color: #8a2be2;
+}
+
+</style>
+
