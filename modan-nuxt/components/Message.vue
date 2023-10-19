f121548569f228908f47c348ecbfdc5f8ed6525f
kijima masaru <kijima4510@gmail.com>
2023-10-19 15:44:29 +0900

    画面投稿ページの作成(index.vue/SideNav.vue/Message.vue)

---
+++ modan-nuxt/components/Message.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="message">
+    <div class="header">
+      <div class="title">{{ username }}</div>
+      <div class="icons">
+        <span>❤️ {{ likes }}</span>
+        <span>✖️</span>
+      </div>
+    </div>
+    <div class="content">{{ content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: {
+    username: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    likes: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.message {
+  margin-bottom: 20px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.icons span {
+  margin-left: 10px;
+  cursor: pointer;
+}
+</style>
--- modan-nuxt/components/SideNav.vue
+++ modan-nuxt/components/SideNav.vue
@@ -1,82 +1,36 @@
 <template>
-  <div class="side-nav">
-    <div class="title">
-      SHARE
+  <div class="sidebar">
+    <h1>SHARE</h1>
+    <div class="menu-item">ホーム</div>
+    <div class="menu-item">ログアウト</div>
+    <div class="post-box">
+      <textarea placeholder="シェア"></textarea>
+      <button>シェアする</button>
     </div>
-    <div>
-      <router-link to="/">ホーム</router-link>
-    </div>
-    <button @click="logout">ログアウト</button>
-    <textarea v-model="postContent" placeholder="投稿内容"></textarea>
-    <button @click="submitPost">シェアする</button>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      postContent: ''
-    };
-  },
-  methods: {
-    logout() {
-      // ログアウト処理
-    },
-    submitPost() {
-      // 投稿内容をサーバーに送信する処理
-    }
-  }
+  name: 'SideNav'
 }
 </script>
 
 <style scoped>
-.side-nav {
-  width: 30%; /* 例として、全体の30%を使用 */
-  background-color: #1A1A2E;
+.sidebar {
+  width: 20%;
+  background-color: #2c3e50;
+  color: white;
   padding: 20px;
-  box-shadow: 2px 0px 5px rgba(0, 0, 0, 0.2);
-  height: 100vh;
 }
 
-.title {
-  font-size: 24px;
-  font-weight: bold;
-  color: #E94560;
-  margin-bottom: 20px;
-}
-
-router-link {
-  display: block;
-  color: #FFF;
-  text-decoration: none;
-  margin-bottom: 10px;
-  font-size: 18px;
-}
-
-button {
-  display: block;
-  background-color: #E94560;
-  color: #FFF;
-  border: none;
-  padding: 10px 20px;
-  margin-top: 20px;
-  border-radius: 5px;
+.menu-item {
+  margin: 20px 0;
   cursor: pointer;
-  transition: background-color 0.3s;
 }
 
-button:hover {
-  background-color: #D43F50;
-}
-
-textarea {
-  width: 100%;
-  height: 120px;
-  padding: 10px;
-  margin-top: 20px;
-  border: none;
-  border-radius: 5px;
-  resize: none;
+.post-box {
+  margin-top: 50px;
 }
 </style>
+
