ce2808d02f1a87097c4d57d0eec10626d9ea02b9
kijima masaru <kijima4510@gmail.com>
2023-10-19 20:32:28 +0900

    投稿画面ページ(posts_id.vue)において、ログアウトするとindex.vueにリダイレクトするように修正

--- modan-nuxt/components/SideNav.vue
+++ modan-nuxt/components/SideNav.vue
@@ -2,7 +2,7 @@
   <div class="sidebar">
     <h1>SHARE</h1>
     <div class="menu-item">ホーム</div>
-    <div class="menu-item">ログアウト</div>
+    <div class="menu-item" @click="logout">ログアウト</div>
     <div class="post-box">
       <textarea placeholder="シェア"></textarea>
       <button>シェアする</button>
@@ -11,8 +11,20 @@
 </template>
 
 <script>
+import firebase from '~/plugins/firebase'
 export default {
-  name: 'SideNav'
+  name: 'SideNav',
+  methods: {
+    logout() {
+      firebase.auth().signOut()
+        .then(() => {
+          this.$router.push('/');
+        })
+        .catch((error) => {
+          console.error('ログアウトに失敗しました:', error);
+        });
+    }
+  }
 }
 </script>
 
